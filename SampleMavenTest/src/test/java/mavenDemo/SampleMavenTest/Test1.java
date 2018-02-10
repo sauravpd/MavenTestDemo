5d005abc1b6a3e17eab31ae422e4197cb7e88251
saurav <sauravpd58@gmail.com>
2018-02-11 00:50:43 +0530

    initial commit

---
+++ SampleMavenTest/src/test/java/mavenDemo/SampleMavenTest/Test1.java
@@ -0,0 +1,24 @@
+package mavenDemo.SampleMavenTest;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.testng.annotations.Test;
+
+/**
+ * Maven Sample
+ * 
+ * @author Saurav
+ *
+ */
+public class Test1 {
+	public static Logger LOG = Logger.getLogger(Test1.class);
+
+	@Test
+	public void test() {
+		PropertyConfigurator.configure(
+				System.getProperty("user.dir") + File.separator + "resources" + File.separator + "log4j.properties");
+		LOG.info("test");
+	}
+}
